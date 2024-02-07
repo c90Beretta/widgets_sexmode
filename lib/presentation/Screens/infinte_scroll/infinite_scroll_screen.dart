f94fb4afd386f6e0be843e2ae07b9d1e7bf8d659
BRN <brayanadanromeronavarro@gmail.com>
2024-02-06 18:57:48 -0700

    Infinite Scroll on work

--- lib/presentation/Screens/infinte_scroll/infinite_scroll_screen.dart
+++ lib/presentation/Screens/infinte_scroll/infinite_scroll_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-class InfiniteScrollScreen extends StatelessWidget {
+import 'package:go_router/go_router.dart';
+class InfiniteScrollScreen extends StatefulWidget {
 
   static const name = "Infinite_Scroll_Screen";   
 
@@ -7,7 +8,69 @@ class InfiniteScrollScreen extends StatelessWidget {
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+
+  List<int> imagesIds = [
+    1,2,3,4,5
+  ];
+
+  final ScrollController scrollController = ScrollController();
+
+
+@override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >= scrollController.position.maxScrollExtent - 500) {
+        // load Next Page
+        addFiveImages();
+      }
+    });
+  
+    }
+
+    @override
+    void dispose() {
+
+      super.dispose();
+    }
+
+
+  void addFiveImages(){
+    final lastId = imagesIds.last;
+
+    imagesIds.addAll([1,2,3,4,5].map((e) => lastId + e));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          // controller: ??,
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return  FadeInImage(
+              width: double.infinity,
+              height: 300 ,
+              fit: BoxFit.cover,
+              placeholder: const AssetImage("assets/image/jar-loading.gif"), 
+              image: NetworkImage("https://picsum.photos/id/${imagesIds[index]}/500/300"),
+              );
+          },
+          ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: const Icon(Icons.arrow_back_ios_new_rounded)),
+    );
   }
 }
